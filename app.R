c7cec56a811a37ea07d95b8d293faeb9cb3a0825
rnstrader <rnstrade@ncsu.edu>
2025-10-20 17:52:19 -0400

    comm

--- app.R
+++ app.R
@@ -95,77 +95,72 @@ server <- function(input, output, session) {
     })
     
 
+    #creating sample_corr, a reactiveValues object that has two elements that default to null
+    sample_corr <- reactiveValues(corr_data = NULL, corr_truth = NULL)
 
+    
+    #Uncomment the next large block of code to go in an
+    #observeEvent() to look for the action button (corr_sample)
+    #Note you can highlight and bulk comment/uncomment (ctrl+shift+c or similar on mac)
 
-    #Create a reactiveValues() object called sample_corr
-    #this object should have two elements, corr_data and corr_truth
-    #both should be set to null to start with!
+      if(input$hhl_corr == "all"){
+        hhl_sub <- HHLvals
+      } else if(input$hhl_corr == "english"){
+        hhl_sub <- HHLvals["1"]
+      } else if(input$hhl_corr == "spanish"){
+        hhl_sub <- HHLvals["2"]
+      } else {
+        hhl_sub <- HHLvals[c("0", "3", "4", "5")]
+      }
 
+      if(input$fs_corr == "all"){
+        fs_sub <- FSvals
+      } else if(input$fs_corr == "yes"){
+        fs_sub <- FSvals["1"]
+      } else {
+        fs_sub <- FSvals["2"]
+      }
 
+      if(input$schl_corr == "all"){
+        schl_sub <- SCHLvals
+      } else if(input$schl_corr == "no_hs"){
+        schl_sub <- SCHLvals[c("0", "01", "02", "03", "04",
+                               "05", "06", "07", "08", "09",
+                               "10", "11", "12", "13", "14", "15")]
+      } else if(input$schl_corr == "hs"){
+        schl_sub <- SCHLvals[as.character(16:19)]
+      } else {
+        schl_sub <- SCHLvals[as.character(20:24)]
+      }
 
-    # ##############################################################
-    # #Uncomment the next large block of code to go in an
-    # #observeEvent() to look for the action button (corr_sample)
-    # #Note you can highlight and bulk comment/uncomment (ctrl+shift+c or similar on mac)
-    # 
-    #   if(input$hhl_corr == "all"){
-    #     hhl_sub <- HHLvals
-    #   } else if(input$hhl_corr == "english"){
-    #     hhl_sub <- HHLvals["1"]
-    #   } else if(input$hhl_corr == "spanish"){
-    #     hhl_sub <- HHLvals["2"]
-    #   } else {
-    #     hhl_sub <- HHLvals[c("0", "3", "4", "5")]
-    #   }
-    # 
-    #   if(input$fs_corr == "all"){
-    #     fs_sub <- FSvals
-    #   } else if(input$fs_corr == "yes"){
-    #     fs_sub <- FSvals["1"]
-    #   } else {
-    #     fs_sub <- FSvals["2"]
-    #   }
-    # 
-    #   if(input$schl_corr == "all"){
-    #     schl_sub <- SCHLvals
-    #   } else if(input$schl_corr == "no_hs"){
-    #     schl_sub <- SCHLvals[c("0", "01", "02", "03", "04",
-    #                            "05", "06", "07", "08", "09",
-    #                            "10", "11", "12", "13", "14", "15")]
-    #   } else if(input$schl_corr == "hs"){
-    #     schl_sub <- SCHLvals[as.character(16:19)]
-    #   } else {
-    #     schl_sub <- SCHLvals[as.character(20:24)]
-    #   }
-    # 
-    #   corr_vars <- c(input$corr_x, input$corr_y)
-    # 
-    #   subsetted_data <- my_sample |>
-    #     filter(#cat vars first
-    #       HHLfac %in% hhl_sub,
-    #       FSfac %in% fs_sub,
-    #       SCHLfac %in% schl_sub
-    #     ) %>% #make sure numeric variables are in appropriate range, must use %>% here for {} to work
-    #     {if("WKHP" %in% corr_vars) filter(., WKHP > 0) else .} %>%
-    #     {if("VALP" %in% corr_vars) filter(., !is.na(VALP)) else .} %>%
-    #     {if("TAXAMT" %in% corr_vars) filter(., !is.na(TAXAMT)) else .} %>%
-    #     {if("GRPIP" %in% corr_vars) filter(., GRPIP > 0) else .} %>%
-    #     {if("GASP" %in% corr_vars) filter(., GASP > 0) else .} %>%
-    #     {if("ELEP" %in% corr_vars) filter(., ELEP > 0) else .} %>%
-    #     {if("WATP" %in% corr_vars) filter(., WATP > 0) else .} %>%
-    #     {if("PINCP" %in% corr_vars) filter(., AGEP > 18) else .} %>%
-    #     {if("JWMNP" %in% corr_vars) filter(., !is.na(JWMNP)) else .}
-    # 
-    #   index <- sample(1:nrow(subsetted_data),
-    #                   size = input$corr_n,
-    #                   replace = TRUE,
-    #                   prob = subsetted_data$PWGTP/sum(subsetted_data$PWGTP))
-    #   #***You now need to update the sample_corr reactive value object***
-    #   #the corr_data argument should be updated to be the subsetted_data[index,]
-    #   #the corr_truth argument should be updated to be the correlation between
-    #   #the two variables selected. This can be found with this code:
-    #   #cor(sample_corr$corr_data |> select(corr_vars))[1,2]
-    ####################################################################
+      corr_vars <- c(input$corr_x, input$corr_y)
+
+      subsetted_data <- my_sample |>
+        filter(#cat vars first
+          HHLfac %in% hhl_sub,
+          FSfac %in% fs_sub,
+          SCHLfac %in% schl_sub
+        ) %>% #make sure numeric variables are in appropriate range, must use %>% here for {} to work
+        {if("WKHP" %in% corr_vars) filter(., WKHP > 0) else .} %>%
+        {if("VALP" %in% corr_vars) filter(., !is.na(VALP)) else .} %>%
+        {if("TAXAMT" %in% corr_vars) filter(., !is.na(TAXAMT)) else .} %>%
+        {if("GRPIP" %in% corr_vars) filter(., GRPIP > 0) else .} %>%
+        {if("GASP" %in% corr_vars) filter(., GASP > 0) else .} %>%
+        {if("ELEP" %in% corr_vars) filter(., ELEP > 0) else .} %>%
+        {if("WATP" %in% corr_vars) filter(., WATP > 0) else .} %>%
+        {if("PINCP" %in% corr_vars) filter(., AGEP > 18) else .} %>%
+        {if("JWMNP" %in% corr_vars) filter(., !is.na(JWMNP)) else .}
+
+      index <- sample(1:nrow(subsetted_data),
+                      size = input$corr_n,
+                      replace = TRUE,
+                      prob = subsetted_data$PWGTP/sum(subsetted_data$PWGTP))
+      #***You now need to update the sample_corr reactive value object***
+      #the corr_data argument should be updated to be the subsetted_data[index,]
+      #the corr_truth argument should be updated to be the correlation between
+      #the two variables selected. This can be found with this code:
+      #cor(sample_corr$corr_data |> select(corr_vars))[1,2]
+    ###################################################################
 
 
 
